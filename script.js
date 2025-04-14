a3c89ecea76b2ab4fe828ef1f2174701be85bb0e
みず <pontanter@github.com>
2025-04-14 19:54:32 +0200

    animation

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-function frog() {
-    console.log("click")
-}
+const frog = document.querySelector('div.frog');
+let frogBusy = false;
+
+frog.onclick = () => {
+    if (frogBusy) return;
+    frogBusy = true;
+    frog.style.transform = 'rotate(360deg)';
+    setTimeout(_ => {
+	frog.classList.remove('transition');
+	requestAnimationFrame(_ => {
+	    frog.style = '';
+	    requestAnimationFrame(_ => {
+		frog.classList.add('transition');
+		frogBusy = false;
+	    });
+	});
+    }, 450);
+};
